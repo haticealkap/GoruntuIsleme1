8dcd78b79d863285446f8f4fc334a9d823a042d6
haticealkap <haticealkap46@gmail.com>
2021-11-01 16:17:17 +0300

    ilk commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿
+namespace EmguCV_Shapes
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(808, 482);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(826, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Resim Seç";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(826, 72);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Renkleri Getir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(826, 157);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 28);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(826, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Renkler";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(826, 204);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 44);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Seç ve Say";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1021, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Şekil toplam sayısı:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1161, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(1021, 72);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(187, 20);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Seçilen renkteki şekil sayısı:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1214, 72);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(17, 20);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1021, 120);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(228, 20);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Seçilen şekildeki farklı renk sayısı:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(1255, 120);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(17, 20);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "0";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(1021, 190);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(236, 58);
+            this.button4.TabIndex = 17;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1021, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Seçilen renk";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1373, 583);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,201 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmguCV_Shapes
+{
+    public partial class Form1 : Form
+    {
+        int toplamSekilSayisi = 0;
+        int secilenRenktekiSekilSayisi = 0;
+        int secilenSekildeFarkliRenkSayisi = 0;
+
+
+        Image<Bgr, byte> rgbImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawContour(Image<Gray, byte> src)
+        {
+            using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+            {
+                //CvInvoke.FindContours(src, contours, null, RetrType.External,
+                //                        ChainApproxMethod.ChainApproxSimple);
+                CvInvoke.FindContours(src, contours, null, RetrType.External,
+                       ChainApproxMethod.ChainApproxSimple);
+                int count = contours.Size;
+                for (int i = 0; i < count; i++)
+                {
+                    using (VectorOfPoint contour = contours[i])
+                    using (VectorOfPoint approxContour = new VectorOfPoint())
+                    {
+
+                        CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.05, true);
+                        if (CvInvoke.ContourArea(approxContour, false) > 250) //only consider contours with area greater than 250
+                        {
+                            if (approxContour.Size == 4) //The contour has 4 vertices.
+                            {
+
+                                toplamSekilSayisi++;
+                                #region determine if all the angles in the contour are within [80, 100] degree
+                                bool isRectangle = true;
+                                Point[] pts = approxContour.ToArray();
+                                LineSegment2D[] edges = PointCollection.PolyLine(pts, true);
+
+                                for (int j = 0; j < edges.Length; j++)
+                                {
+                                    double angle = Math.Abs(
+                                        edges[(j + 1) % edges.Length].GetExteriorAngleDegree(edges[j]));
+                                    if (angle < 80 || angle > 100)
+                                    {
+                                        isRectangle = false;
+                                        break;
+                                    }
+                                }
+                                if (isRectangle)
+                                {
+                                    #endregion
+                                    // Original contour
+
+
+                                    // CvInvoke.DrawContours(rgbImage, contours, i, new MCvScalar(255, 0, 255, 255), 3);
+
+
+                                    // approximate contour
+                                    CvInvoke.ApproxPolyDP(contour, approxContour, CvInvoke.ArcLength(contour, true) * 0.02, true);
+                                    //Bulunan nesnenin orta noktasını bulmak için
+                                    int totalX = 0;
+                                    int totalY = 0;
+                                    for (int x = 0; x < pts.Length; x++)
+                                    {
+                                        Point p1 = new Point(pts[x].X, pts[x].Y);
+                                        totalX += p1.X;
+                                        totalY += p1.Y;
+                                    }
+
+                                    string hexCode = getHexCode(rgbImage, totalY / 4, totalX / 4);
+                                    if (comboBox1.SelectedItem != null && hexCode == comboBox1.SelectedItem.ToString())
+                                    {
+                                        //Çizim işlemi yaptırmak için
+                                        for (int j = 0; j < pts.Length; j++)
+                                        {
+                                            Point p1 = new Point(pts[j].X, pts[j].Y);
+
+                                            Point p2;
+                                            if (j == pts.Length - 1)
+                                                p2 = new Point(pts[0].X, pts[0].Y);
+                                            else
+                                                p2 = new Point(pts[j + 1].X, pts[j + 1].Y);
+
+
+                                            CvInvoke.Line(rgbImage, p1, p2, new MCvScalar(255, 0, 0, 0), 3);
+                                        }
+                                        secilenRenktekiSekilSayisi++;
+                                    }
+                                    else
+                                    {
+                                        secilenSekildeFarkliRenkSayisi++;
+                                    }
+                                }
+
+
+                            }
+                        }
+
+
+                    }
+                }
+            }
+        }
+        string getHexCode(Image<Bgr, byte> src, int x, int i)
+        {
+            Color myColor = Color.FromArgb(Convert.ToInt32(src[x, i].Red), Convert.ToInt32(src[x, i].Green), Convert.ToInt32(src[x, i].Blue));
+            return myColor.R.ToString("X2") + myColor.G.ToString("X2") + myColor.B.ToString("X2");
+        }
+
+        public List<string> getColors(Image<Bgr, byte> src)
+        {
+            List<string> colors = new List<string>();
+
+            for (int i = 0; i < src.Width; i++)
+            {
+                for (int x = 0; x < src.Height; x++)
+                {
+
+                    string hex = getHexCode(src, x, i);
+
+                    if (colors.IndexOf(hex) == -1 && hex != "FFFFFF")
+                    {
+                        colors.Add(hex);
+                    }
+                }
+            }
+            return colors;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                //Get the path of specified file
+                string filePath = openFileDialog1.FileName;
+
+                rgbImage = new Image<Bgr, byte>(filePath);
+
+                Image i = Image.FromStream(File.OpenRead(openFileDialog1.FileName));
+                pictureBox1.Image = i;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> unprocessedImage = new Image<Bgr, byte>(openFileDialog1.FileName);
+            List<string> colors = getColors(unprocessedImage);
+            comboBox1.Items.Clear();
+            for (int i = 0; i < colors.Count; i++)
+            {
+
+                comboBox1.Items.Add(colors[i]);
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(openFileDialog1.FileName))
+            {
+                toplamSekilSayisi = 0;
+                secilenRenktekiSekilSayisi = 0;
+                secilenSekildeFarkliRenkSayisi = 0;
+
+                Image<Gray, byte> grayImage = new Image<Gray, byte>(openFileDialog1.FileName);
+
+                Image<Gray, byte> CannyImage = grayImage.Clone();
+                CvInvoke.Canny(grayImage, CannyImage, 255, 255, 5, true);
+                DrawContour(CannyImage);
+                rgbImage.Save("processedImage.jpg");
+                Image i = Image.FromStream(File.OpenRead("processedImage.jpg"));
+                pictureBox1.Image = i;
+                label4.Text = toplamSekilSayisi.ToString();
+                label6.Text = secilenRenktekiSekilSayisi.ToString();
+                label8.Text = secilenSekildeFarkliRenkSayisi.ToString();
+                button4.BackColor = (Color)new ColorConverter().ConvertFromString("#" + comboBox1.SelectedItem.ToString());
+
+
+            }
+        }
+    }
+}
